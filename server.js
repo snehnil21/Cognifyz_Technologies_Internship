bc52030d9cf8332a391e8aacc5eb49101f5e21a2
SnehNil21 <snehalbhojraj2001@gmail.com>
2025-01-08 13:40:54 +0530

    update server.js file

--- server.js
+++ server.js
@@ -2,27 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// Set up the view engine (EJS)
 app.set('view engine', 'ejs');
-
-// Serve static files (like CSS)
 app.use(express.static('public'));
-
-// Body parser middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Route to display the form
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Route to handle form submission
 app.post('/submit', (req, res) => {
-    const { name, email } = req.body; // Access form data
-    res.render('result', { name, email }); // Send data to result.ejs
+    const { name, email } = req.body;
+    res.render('result', { name, email });
 });
 
-// Start the server
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
